9dadc5b34703fdd22606bc606e86ddd16cc3b58e
Fan Fan Firgiawan <firgiawan.fanfan@gmail.com>
2024-05-03 17:10:20 +0700

    test

--- database/migrations/2024_04_24_062758_create_matakuliahs_table.php
+++ database/migrations/2024_04_24_062758_create_matakuliahs_table.php
@@ -13,13 +13,8 @@ return new class extends Migration
     {
         Schema::create('matakuliahs', function (Blueprint $table) {
             $table->id();
-<<<<<<< HEAD
             $table->unsignedBigInteger('dosen_id');
             $table->foreign('dosen_id')->references('id')->on('nama_dosens')->onDelete('restrict');
-=======
-            // $table->unsignedBigInteger('nama_dosen_id');
-            // $table->foreign('nama_dosen_id')->references('id')->on('nama_dosens')->onDelete('restrict');
->>>>>>> 225cc8a6a544406c0afffd70899101150d28f89a
             $table->unsignedBigInteger('program_studi_id');
             $table->foreign('program_studi_id')->references('id')->on('program_studis')->onDelete('restrict');
             $table->string('kode_matakuliah');
